85441d4d47fba1f3dd5406bbfd7c93be2e65c479
hlypik <arsenyborovskoi@yandex.ru>
2026-01-21 02:34:32 +0300

    исправения

--- core/interaction-api/src/main/java/ru/practicum/feign/location/LocationFeignClient.java
+++ core/interaction-api/src/main/java/ru/practicum/feign/location/LocationFeignClient.java
@@ -2,6 +2,6 @@ package ru.practicum.feign.location;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "location-service", path = "/internal/location")
+@FeignClient(name = "LOCATION-SERVICE", path = "/internal/location")
 public interface LocationFeignClient extends InternalLocationContract {
 }
